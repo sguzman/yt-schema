6f5391949265b98c2276163228ca2e5cb0efcb80
Salvador Guzman <guzmansalv@gmail.com>
2024-05-19 11:51:28 -0700

    chore: Refactor main.py to improve code structure and readability

--- src/yt_schema/main.py
+++ src/yt_schema/main.py
@@ -1,4 +1,39 @@
 import logging
+import json
+
+import peewee as p
+import datetime
+
+
+db = p.SqliteDatabase("yt.db")
+
+
+def load_json():
+    logging.info("load_json")
+    return json.loads(open("resources/channel.json").read())
+
+
+def keys(data):
+    logging.info("keys")
+
+    logging.info("keys: %s", data.keys())
+
+
+class BaseModel(p.Model):
+    class Meta:
+        database = db
+
+
+class User(BaseModel):
+    username = p.CharField(unique=True)
+
+
+class Tweet(BaseModel):
+    user = p.ForeignKeyField(User, backref="tweets")
+    message = p.TextField()
+    created_date = p.DateTimeField(default=datetime.datetime.now)
+    is_published = p.BooleanField(default=True)
+
 
 # Enable timestamp and log level in log messages
 logging.basicConfig(
@@ -8,6 +43,8 @@ logging.basicConfig(
 
 def main():
     logging.info("start")
+    json_data = load_json()
+    keys(json_data)
     logging.info("end")
 
 
