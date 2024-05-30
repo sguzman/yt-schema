c079b8b5ac3a58534c771a994973e65c9b110338
Salvador Guzman <guzmansalv@gmail.com>
2024-05-30 14:19:21 -0700

    Refactor load_json function to use type hints and add docstring

--- src/yt_schema/main.py
+++ src/yt_schema/main.py
@@ -2,12 +2,42 @@ import logging
 import os
 import json
 
+from typing import Dict
 from yt_schema import tables, timeseries
 
 
-def load_json(name: str):
-    logging.info(f"load_json {name}")
-    return json.loads(open(f"resources/{name}").read())
+def load_json(name: str) -> Dict[str, object]:
+    """
+    Load a JSON file from the "resources" directory.
+
+    This function takes a filename as input and reads the corresponding JSON file
+    located in the "resources" directory. It returns the contents of the JSON file
+    as a Python dictionary. If the specified JSON file does not exist in the
+    "resources" directory, it raises a FileNotFoundError.
+
+    Args:
+        name (str): The name of the JSON file to load.
+
+    Returns:
+        Dict[str, object]: The contents of the JSON file as a Python Dictionary.
+
+    Raises:
+        FileNotFoundError: If the specified JSON file does not exist in the "resources" directory.
+    """
+
+    # Construct the path to the JSON file by joining the "resources" directory
+    # with the provided filename.
+    json_file: str = os.path.join("resources", name)
+
+    # Open the JSON file and read its contents. The 'with' statement ensures that
+    # the file is properly closed after reading.
+    with open(json_file, "r") as f:
+        # Load the JSON data from the file and store it in a variable. The json.load()
+        # function reads the file and returns a Python dictionary.
+        json_data: Dict[str, object] = json.load(f)
+
+    # Return the loaded JSON data.
+    return json_data
 
 
 # Enable timestamp and log level in log messages
